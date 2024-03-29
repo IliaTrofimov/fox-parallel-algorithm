6fc6430e1e04be688ac26212a7d7b1793cfbfdb2
Трофимов Илья Сергеевич <istrof10@mts.ru>
2024-03-30 01:01:50 +0300

    Fptl update

--- run_fptl.ps1
+++ run_fptl.ps1
@@ -1,15 +1,24 @@
 param(
-    [Parameter(Mandatory)]$version,
-    [Parameter()]$dbg
+    [Parameter(Mandatory=$true, Position=0)]
+    $script,
 
+    [Parameter(Mandatory=$false, Position=1)]
+    [ValidateRange(1, 16)]
+    [int]$threads = 1,
+
+    [Parameter(Mandatory=$false, Position=2)]
+    $fptlexe = ".fptl/fptl.exe"
 )
 
-Write-Host "RUN fptl_$version : " -ForegroundColor Cyan
+
+#Read more: https://www.sharepointdiary.com/2021/02/powershell-function-parameters.html#ixzz8Vu58LpEi
+
+Write-Host "RUN fptl with ${threads} threads:" -ForegroundColor Cyan
 
 $t0 = Get-Date
-& ".fptl_$version\fptl.exe" "c:\Users\istrof10\source\repos\FoxAlgorithm\fox_fptl.fptl"
+& $fptlexe $script -n $threads
 $t1 = Get-Date
 $dt = New-TimeSpan -Start $t0 -End $t1
 
-Write-Host "FPTL DONE in $dt" -ForegroundColor Cyan
+Write-Host "DONE in $dt" -ForegroundColor Cyan
 Write-Host
