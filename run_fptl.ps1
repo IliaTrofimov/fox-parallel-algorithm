2aaf9ab3aa87393903d4e44e0a22aae9211b2d34
Трофимов Илья Сергеевич <istrof10@mts.ru>
2024-03-30 17:04:52 +0300

    Update fptl + test scrit

--- run_fptl.ps1
+++ run_fptl.ps1
@@ -1,24 +1,12 @@
 param(
     [Parameter(Mandatory=$true, Position=0)]
     $script,
-
-    [Parameter(Mandatory=$false, Position=1)]
-    [ValidateRange(1, 16)]
-    [int]$threads = 1,
-
     [Parameter(Mandatory=$false, Position=2)]
     $fptlexe = ".fptl/fptl.exe"
 )
 
 
-#Read more: https://www.sharepointdiary.com/2021/02/powershell-function-parameters.html#ixzz8Vu58LpEi
-
-Write-Host "RUN fptl with ${threads} threads:" -ForegroundColor Cyan
-
-$t0 = Get-Date
-& $fptlexe $script -n $threads
-$t1 = Get-Date
-$dt = New-TimeSpan -Start $t0 -End $t1
-
-Write-Host "DONE in $dt" -ForegroundColor Cyan
-Write-Host
+for ($threads = 1; $threads -le 8; $threads = $threads + 1){
+    & $fptlexe $script -n $threads
+    Write-Host `t -NoNewline
+}
